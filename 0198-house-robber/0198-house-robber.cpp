44d199bfc8aef7bbad5a2d842a4e72ed62b67969
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2024-10-15 23:16:01 +0530

    Time: 4 ms (32.22%), Space: 9.6 MB (11.53%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -12,9 +12,22 @@ public:
         return profit[index] = max(notPick, pick);
     }
     int rob(vector<int>& nums) {
+        // int size = nums.size();
+        // vector< int > profit(size, -1);
+        // int totalProfit = maximumProfit(nums, size -1, profit);
+        // return totalProfit;
+
         int size = nums.size();
-        vector< int > profit(size, -1);
-        int totalProfit = maximumProfit(nums, size -1, profit);
-        return totalProfit;
+        if(size == 1) return nums[0];
+        vector< int > profit(size, 0);
+        profit[0] = nums[0];
+        profit[1] = max(nums[0], nums[1]);
+
+        for(int index = 2; index < size; index ++){
+            int notPick = profit[index -1];
+            int pick = nums[index] + profit[index - 2];
+            profit[index] = max(notPick, pick);
+        }
+        return profit[size - 1];
     }
 };
