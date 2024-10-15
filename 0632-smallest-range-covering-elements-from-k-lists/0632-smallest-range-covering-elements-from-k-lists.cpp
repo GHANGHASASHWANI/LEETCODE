00ef3c5f6443a2201f21baddcb40d4dc98e47ff5
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2024-10-15 17:07:41 +0530

    Time: 87 ms (21.61%), Space: 29 MB (5.22%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -1,58 +1,36 @@
 class Solution {
 public:
+typedef pair<int, pair<int, int>> pp;
     vector<int> smallestRange(vector<vector<int>>& nums) {
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        
-        int n = nums.size();
-        vector<pair<int,int>> vec;
-        
-        // Step 1: Flatten the input into a single vector of pairs
-        for(int i = 0; i < n; i++) {
-            for(auto& k: nums[i]) {
-                vec.push_back({k, i});
-            }
+        priority_queue< pp, vector<pp>, greater<pp>> pq;
+
+        int maxi = nums[0][0];
+        for(int index = 0; index < nums.size(); index++){
+            maxi = max(nums[index][0], maxi);
+            pq.push({nums[index][0], {index, 0}});
         }
-        
-        // Step 2: Sort the flattened vector
-        sort(vec.begin(), vec.end());
-        
-        unordered_map<int,int> mp;
-        int j = 0;
-        vector<pair<int,int>> check;
-        
-        // Step 3: Use sliding window to find valid ranges
-        for(int i = 0; i < vec.size(); i++) {
-            while(j < vec.size() && mp.size() < n) {
-                mp[vec[j].second]++;
-                j++;
+
+        int start = (pq.top()).first;
+        int end = maxi;
+
+        while(true){
+            int currMin = (pq.top()).first;
+            int currRow = ((pq.top()).second).first;
+            int currCol = ((pq.top()).second).second;
+            pq.pop();
+
+            if(maxi - currMin < end - start){
+                end = maxi;
+                start = currMin;
             }
             
-            if(mp.size() == n) {
-                check.push_back({vec[i].first, vec[j-1].first});
-            }
+            currCol++;
             
-            mp[vec[i].second]--;
-            if(mp[vec[i].second] == 0) mp.erase(vec[i].second);
+            if(nums[currRow].size() == currCol) break;
+            pq.push({nums[currRow][currCol], {currRow, currCol}});
+            maxi = max(maxi, nums[currRow][currCol]);
         }
-        
-        // Step 4: Find the smallest range from valid ranges
-        int mini = INT_MAX;
-        vector<int> ans(2);
-        for(int i = 0; i < check.size(); i++) {
-            if(mini > check[i].second - check[i].first) {
-                mini = check[i].second - check[i].first;
-                ans[0] = check[i].first;
-                ans[1] = check[i].second;
-            }
-            else if(mini == check[i].second - check[i].first) {
-                if(ans[0] > check[i].first) {
-                    ans[0] = check[i].first;
-                    ans[1] = check[i].second;
-                }
-            }
-        }
-        
-        return ans;
+        return {start,end};
+
     }
 };
