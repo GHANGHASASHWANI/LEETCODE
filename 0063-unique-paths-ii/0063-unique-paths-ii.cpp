38293d24d3ca04920e9a0b6c25e00d2acb8fb0bf
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2024-11-28 16:37:55 +0530

    Time: 0 ms (100%), Space: 14 MB (8.19%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,40 +1,25 @@
 class Solution {
 public:
+    vector<vector<int>> dp;
+    int totalWays(vector<vector<int>>&obstacles, int row, int col){
+        if(row >= obstacles.size() || col >= obstacles[0].size() || row < 0 || col < 0) return 0;
+        if(obstacles[row][col] == 1) return 0;
+
+        if(row == obstacles.size() -1 && col == obstacles[0].size() - 1) return 1;
+
+        if(dp[row][col] != -1) return dp[row][col];
+        
+        int right = totalWays(obstacles, row + 1, col);
+        int left = totalWays(obstacles, row, col + 1);
+
+        return dp[row][col] = left + right;
+    }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int rows = obstacleGrid.size(), cols = obstacleGrid[0].size();
-        if ((obstacleGrid[rows - 1][cols - 1] == 1) ||
-            (obstacleGrid[0][0] == 1))
-            return 0;
-        vector<vector<int>> ways(rows, vector<int>(cols, 0));
-
-        // Base case for 0th row
-        for (int rowIndex = 0; rowIndex < rows; rowIndex++) {
-            if (obstacleGrid[rowIndex][0] != 1) {
-                ways[rowIndex][0] = 1;
-            } else {
-                break;
-            }
-        }
-        // Base case for 0th column
-        for (int colIndex = 0; colIndex < cols; colIndex++) {
-            if (obstacleGrid[0][colIndex] != 1) {
-                ways[0][colIndex] = 1;
-            } else {
-                break;
-            }
-        }
-
-        for (int rowIndex = 1; rowIndex < rows; rowIndex++) {
-            for (int colIndex = 1; colIndex < cols; colIndex++) {
-                if (obstacleGrid[rowIndex][colIndex] == 1) {
-                    ways[rowIndex][colIndex] = 0; // To prevent use of the path
-                } else {
-                    ways[rowIndex][colIndex] = ways[rowIndex - 1][colIndex] +
-                                               ways[rowIndex][colIndex - 1];
-                }
-            }
-        }
-
-        return ways[rows - 1][cols - 1];
+        int row = obstacleGrid.size() -1;
+        int col = obstacleGrid[0].size() -1;
+        dp.clear();
+        dp.resize(105, vector<int> (105, -1));
+        if(obstacleGrid[row][col] == 1) return 0;
+        return totalWays(obstacleGrid, 0, 0);
     }
 };
