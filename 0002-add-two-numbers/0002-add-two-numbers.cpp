a3eb195e1f3ffaa0b7e3a8e9855a9f7f5c0de1c9
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-01-18 10:58:14 +0530

    Time: 0 ms (100%), Space: 77.2 MB (49.62%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -1,28 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
+
+    void add(ListNode* &l1, ListNode* &l2, int &carry, ListNode* &dummy){
+        if(!l1 || !l2) return; 
+        int sum =  l1->val + l2->val + carry;
+         carry = sum / 10;
+        ListNode* temp = new ListNode(sum % 10);
+
+        dummy->next = temp;
+        dummy = dummy->next;
+        l1 = l1->next;
+        l2 = l2->next;
+        add(l1, l2, carry, dummy);
+    }
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* t1 = l1;
-        ListNode* t2 = l2;
+        ListNode* dummy = new ListNode(-1);
+        ListNode* ans = dummy;
         int carry = 0;
-        ListNode* dummy = new ListNode(0); // Dummy node
-        ListNode* current = dummy; // Pointer to traverse 
-
-        // Traverse both lists
-        while (t1 || t2 || carry) {
-            int sum = carry;
-            if (t1) {
-                sum += t1->val;
-                t1 = t1->next;
-            }
-            if (t2) {
-                sum += t2->val;
-                t2 = t2->next;
-            }
-            carry = sum / 10; // Update the carry 
-            current->next = new ListNode(sum % 10); // Create new node
-            current = current->next; // Move to the next node
+        add(l1, l2, carry, dummy);
+        cout<<"ok1";
+        while(l1){
+            int sum = l1->val + carry;
+             carry = sum / 10;
+            ListNode* temp = new ListNode(sum % 10);
+            dummy->next = temp;
+            dummy = dummy->next;
+            l1 = l1->next;
         }
-
-        return dummy->next; // Return the head next to the dummy value -1.
+        while(l2){
+            int sum = l2->val + carry;
+            carry = sum / 10;
+            ListNode* temp = new ListNode(sum % 10);
+            dummy->next = temp;
+            dummy = dummy->next;
+            l2 = l2->next;
+        }
+        if(carry){
+            ListNode* temp = new ListNode(carry);
+            dummy->next = temp;
+        }
+        return ans->next;
     }
 };
