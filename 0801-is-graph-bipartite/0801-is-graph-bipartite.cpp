7d1031f43ed1413dc14dd556585f425cc717eff2
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-01-30 17:04:52 +0530

    Time: 3 ms (16.7%), Space: 17.2 MB (75.98%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,21 +1,37 @@
 class Solution {
 public:
 
-    bool checkGraph(vector<vector<int>>& graph, vector<int> &color, int node){
-        queue<int> qu;
-        qu.push(node);
-        color[node] = 0;
-        while(qu.size()){
-            int temp = qu.front();
-            qu.pop();
-            for(auto it: graph[temp]){
-                if(color[it] == -1){
-                    color[it] = !color[temp];
-                    qu.push(it);
-                }
-                else if (color[it] == color[temp]){
-                    return false;
-                }
+    // bool checkGraph(vector<vector<int>>& graph, vector<int> &color, int node){
+    //     queue<int> qu;
+    //     qu.push(node);
+    //     color[node] = 0;
+    //     while(qu.size()){
+    //         int temp = qu.front();
+    //         qu.pop();
+    //         for(auto it: graph[temp]){
+    //             if(color[it] == -1){
+    //                 color[it] = !color[temp];
+    //                 qu.push(it);
+    //             }
+    //             else if (color[it] == color[temp]){
+    //                 return false;
+    //             }
+    //         }
+    //     }
+    //     return true;
+    // }
+
+    bool checkGraph(vector<vector< int >> &graph, vector< int > &color, int current, int previousColor){
+
+        for(auto node : graph[current]){
+            if(color[node] == -1){
+                color[node] = not color[current];
+                // cout<<"color seq "<<node << " " << color[node]<< endl;
+                if( not checkGraph(graph, color, node, color[node])) return false;
+            }
+            else if(color[node] == color[current]){
+                // cout<<" stopped "<< node <<" "<< current<<" " << color[node] << endl;
+                return false;
             }
         }
         return true;
@@ -28,7 +44,8 @@ public:
         
         for(int i=0;i<n;i++){
             if(color[i] == -1){
-                bool ans = checkGraph(graph,color,i);
+                color[i] = 0;
+                bool ans = checkGraph(graph,color,i, 0);
                 if(!ans) return false;
             }
         }
