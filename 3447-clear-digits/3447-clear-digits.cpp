c79c96a7ab95db1782dae2c941e03ff8b5f197c5
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-02-10 22:05:09 +0530

    Time: 0 ms (100%), Space: 9.2 MB (14.68%) - LeetHub

---
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        stack< char > st;
+        for(int i =0; i < s.size(); i++){
+            if(!st.empty() && (s[i]) >= '0' && (s[i]) <= '9'){
+                st.pop();
+            }
+            else{
+                st.push(s[i]);
+            }
+        }
+        string ans = "";
+        while(!st.empty()){
+            ans =  st.top() + ans;
+            st.pop();
+        }
+        return ans;
+    }
+};
