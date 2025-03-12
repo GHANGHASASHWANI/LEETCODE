389304ed9a0c3c2be3a0abe2ee8535021261de48
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-03-12 07:14:44 +0530

    Time: 2 ms (4.16%), Space: 21.5 MB (10.71%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,12 +1,48 @@
 class Solution {
 public:
-    int maximumCount(vector<int>& nums) {
-        int pos = 0, neg = 0;
 
-        for(int i =0; i < nums.size(); i++){
-            if(nums[i] < 0) neg++;
-            if(nums[i] > 0) pos++;
+    int getNegIndex(vector<int> &arr){
+        int low =0, high = arr.size() - 1, mid = 0;
+        int ans = -1;
+        while(low <= high){
+            mid = low + (high - low)/2;
+
+            if(arr[mid] < 0){
+                ans = mid;
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
         }
-        return max(neg, pos);
+        return ans;
+    }
+
+    int getPosIndex(vector<int> &arr){
+        int low =0, high = arr.size() - 1, mid = 0;
+        int ans = -1;
+        while(low <= high){
+            mid = low + (high - low)/2;
+
+            if(arr[mid] > 0){
+                ans = mid;
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    int maximumCount(vector<int>& nums) {
+        int pos = getPosIndex(nums), neg = getNegIndex(nums);
+        if(pos != -1){
+            pos = nums.size() - pos;
+        }
+        cout<<pos<<" "<<neg<<endl;
+        if(neg != -1){
+            neg = neg + 1;
+        }
+        if(pos == -1 && neg == -1)return 0;
+        return max(pos,neg);
     }
 };
