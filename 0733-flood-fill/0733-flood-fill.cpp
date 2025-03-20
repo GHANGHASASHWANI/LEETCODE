370ad6c2af9bbb1bcae6698177f5c661bf5d969c
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-03-20 21:45:26 +0530

    Time: 0 ms (100%), Space: 18.3 MB (34.75%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,25 +1,39 @@
 class Solution {
 public:
-void dfs(vector<vector< int >> &image, int sr, int sc, int color, int need, vector<vector<int>> &visited){
-        image[sr][sc] = color;
-        visited[sr][sc] = 1;
-        int delrow[] = {0,1,0,-1};
-        int delcol[] = {1,0,-1,0};
+    int r[4] = {0, 0, 1, -1};
+    int c[4] = {1, -1, 0, 0};
+    void BFS(vector<vector< int >> &answer, vector<vector<int>> &image, int sr, int sc, int newColor, int oldColor, set< pair<int, int>> &visited){
+        queue< pair< int, int >> qu;
+        qu.push({sr, sc});
+        while(!qu.empty()){
+            int size = qu.size();
 
-        for(int i=0;i<4;i++){
-            int row = sr + delrow[i];
-            int col = sc + delcol[i];
-            if(row>=0 && row< image.size() && col >=0 && col < image[0].size() && visited[row][col] == -1 && image[row][col] == need){
-                dfs(image, row, col, color, need, visited);
+            for(int i =0; i < qu.size(); i++){
+                int row = qu.front().first;
+                int col = qu.front().second;
+                qu.pop();
+                answer[row][col] = newColor;
+                for(int i =0; i < 4; i++){
+                    int newRow = row + r[i];
+                    int newCol = col + c[i];
+
+                    if(newRow >= 0 && newRow < image.size() && newCol >= 0 && newCol < image[0].size() && image[newRow][newCol] == oldColor && !visited.count({newRow, newCol})){
+                        qu.push({newRow, newCol});
+                        visited.insert({newRow, newCol});
+                    }
+                }
+
             }
         }
-}
+    }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int need = image[sr][sc];
-        int m = image.size();
-        int n = image[0].size();
-        vector<vector<int>> visited(m, vector<int> (n, -1));
-        dfs(image, sr, sc, color, need, visited);
-        return image;
+        vector<vector< int >> answer(image.begin(), image.end());
+        set< pair<int, int>> visited;
+
+        int oldColor = image[sr][sc];
+        BFS(answer, image, sr, sc,color, oldColor, visited);
+
+        return answer;
+
     }
 };
