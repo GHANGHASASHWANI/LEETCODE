26a934468d85210a92969b626d2cafa7c1e6f625
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-04-02 15:13:21 +0530

    Time: 0 ms (100%), Space: 8.5 MB (33.75%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
-    vector< int > dp;
-    int fn(int steps, int n){
-        if(steps == n){
-            return 1;
-        }
-        if(steps > n){
-            return 0;
-        }
-        if(dp[steps] != -1) return dp[steps];
+    // vector< int > dp;
+    // int fn(int steps, int n){
+    //     if(steps == n){
+    //         return 1;
+    //     }
+    //     if(steps > n){
+    //         return 0;
+    //     }
+    //     if(dp[steps] != -1) return dp[steps];
 
-        int step1 = fn(steps + 1, n);
-        int step2 = fn(steps + 2, n);
+    //     int step1 = fn(steps + 1, n);
+    //     int step2 = fn(steps + 2, n);
 
-        return dp[steps] = step1 + step2;
-    }
+    //     return dp[steps] = step1 + step2;
+    // }
     int climbStairs(int n) {
-        int steps =  0;
-        dp.clear();
-        dp.resize(n+1, -1);
-        return fn(steps, n);
+        // int steps =  0;
+        // dp.clear();
+        // dp.resize(n+1, -1);
+        // return fn(steps, n);
+
+        vector<int> dp(n+1, 0);
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
