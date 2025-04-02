03b4d236dc8a859973b3f4d5dc2d49e7dc31edcf
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-04-02 17:46:34 +0530

    Time: 1496 ms (7.4%), Space: 263.8 MB (24.54%) - LeetHub

---
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        int n = nums.size();
+        vector< map< long long, long long> > dp(n);
+        long long result = 0;
+        for(int i =0; i < n; i++){
+            for(int j = 0; j < i; j++){
+                long long diff = (long long)(nums[i]) - (long long)(nums[j]);
+                dp[i][diff] += (1 + dp[j][diff]);
+                result += (1 + dp[j][diff]);
+            }
+        }
+
+        return result - (n*(n-1))/2;
+    }
+};
