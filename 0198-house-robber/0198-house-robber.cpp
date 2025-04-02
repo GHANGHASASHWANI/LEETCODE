c588d6fc7dc41247a5170add313e77a8fa6308bd
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-04-02 10:42:58 +0530

    Time: 0 ms (100%), Space: 10.6 MB (61.75%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,21 +1,7 @@
 class Solution {
 public:
 
-    int maximumProfit(vector< int > &nums, int index, vector< int > &profit){
-        if(index == 0) return nums[0];
-        if(index < 0) return 0;
-
-        if(profit[index] != -1) return profit[index];
-
-        int notPick = maximumProfit(nums, index - 1, profit);
-        int pick = nums[index] + maximumProfit(nums, index - 2, profit);
-        return profit[index] = max(notPick, pick);
-    }
     int rob(vector<int>& nums) {
-        // int size = nums.size();
-        // vector< int > profit(size, -1);
-        // int totalProfit = maximumProfit(nums, size -1, profit);
-        // return totalProfit;
 
         int size = nums.size();
         if(size == 1) return nums[0];
