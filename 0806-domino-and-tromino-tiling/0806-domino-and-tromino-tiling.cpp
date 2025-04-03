834031a465c15f7c053f9a43a90c58a1c702c1d5
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-04-03 14:11:33 +0530

    Time: 0 ms (100%), Space: 9 MB (53.57%) - LeetHub

--- 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.cpp
+++ 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
-vector<int> dp;
+// vector<int> dp;
 int mod = 1e9 + 7;
-    int fn(int n){
-        if(n <= 0) return 0;
-        if(n == 1) return 1;
-        if(n == 2) return 2;
-        if(n == 3) return 5;
+//     int fn(int n){
+//         if(n <= 0) return 0;
+//         if(n == 1) return 1;
+//         if(n == 2) return 2;
+//         if(n == 3) return 5;
 
-        if(dp[n] != -1) return dp[n];
+//         if(dp[n] != -1) return dp[n];
 
-        return dp[n] = (2*fn(n-1)%mod + fn(n-3)%mod)%mod;
-    }
+//         return dp[n] = (2*fn(n-1)%mod + fn(n-3)%mod)%mod;
+//     }
     int numTilings(int n) { 
-        dp.clear();
-        dp.resize(n+1, -1);
-        return fn(n);
+        // dp.clear();
+        // dp.resize(n+1, -1);
+        // return fn(n);
+        if(n <= 2) return n;
+
+        vector<int> dp(n+1, 0);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 5;
+
+        for(int i = 4; i <= n; i++){
+            dp[i] =(2*dp[i-1]%mod + dp[i-3])%mod;
+            // cout<<dp[i]<<endl;
+        }
+        return dp[n];
     }
 };
