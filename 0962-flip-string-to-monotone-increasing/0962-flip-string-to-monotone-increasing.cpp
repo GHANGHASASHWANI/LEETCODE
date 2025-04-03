5ad8cd4519c8a69a910a2ca0164a843035e3f7bf
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-04-03 16:55:01 +0530

    Time: 213 ms (17.45%), Space: 89.2 MB (20.05%) - LeetHub

---
+++ 0962-flip-string-to-monotone-increasing/0962-flip-string-to-monotone-increasing.cpp
@@ -0,0 +1,35 @@
+class Solution {
+private:
+    vector<vector< int >> dp;
+    int fn(int curr, char prev, string &s){
+        if(curr >= s.size()) return 0;
+
+        if(dp[curr][prev - '0'] != -1) return dp[curr][prev - '0'];
+
+        int flip = INT_MAX, nonFlip = INT_MAX;
+
+        if(s[curr] == '1'){
+            if(prev == '0'){
+                flip = 1 + fn(curr+1, '0', s);
+                nonFlip = fn(curr+1, '1', s);
+            }else{
+                nonFlip = fn(curr+1, '1', s);
+            }
+        }else{
+            if(prev == '0'){
+                flip = 1 + fn(curr+1, '1', s);
+                nonFlip = fn(curr+1, '0', s);
+            }else{
+                flip = 1 + fn(curr+1, '1', s);
+            }
+        }
+        return dp[curr][prev - '0'] = min(flip, nonFlip);
+    }
+public:
+    int minFlipsMonoIncr(string s) {
+        int n = s.size();
+        dp.clear();
+        dp.resize(n+1, vector<int> (2, -1));
+        return fn(0, '0', s);
+    }
+};
