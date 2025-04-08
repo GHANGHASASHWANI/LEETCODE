9889c713d702fb59d200974e892193c3bfc790ba
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-04-08 22:13:00 +0530

    Time: 99 ms (92.97%), Space: 34.2 MB (52.93%) - LeetHub

--- 1393-maximum-value-of-k-coins-from-piles/1393-maximum-value-of-k-coins-from-piles.cpp
+++ 1393-maximum-value-of-k-coins-from-piles/1393-maximum-value-of-k-coins-from-piles.cpp
@@ -15,27 +15,27 @@ private:
     //     return dp[idx][j][k] = ans;
     // }
 
-    vector<vector< int >> dp;
+    // vector<vector< int >> dp;
 
-    int fn(int i, vector<vector< int >> &arr, int k){
-        if(i >= arr.size() || k <= 0) return 0;
+    // int fn(int i, vector<vector< int >> &arr, int k){
+    //     if(i >= arr.size() || k <= 0) return 0;
 
-        if(dp[i][k] != -1) return dp[i][k];
+    //     if(dp[i][k] != -1) return dp[i][k];
 
-        int ans = fn(i +1, arr, k);
+    //     int ans = fn(i +1, arr, k);
 
-        int sum = 0;
+    //     int sum = 0;
 
-        for(int j = 0; j < min(k, (int)arr[i].size()); j++){
-            sum += arr[i][j];
-            ans = max(ans, sum + fn(i+1, arr, k - (j+1)));
-        }
+    //     for(int j = 0; j < min(k, (int)arr[i].size()); j++){
+    //         sum += arr[i][j];
+    //         ans = max(ans, sum + fn(i+1, arr, k - (j+1)));
+    //     }
 
-        return dp[i][k] = ans;
-    }
+    //     return dp[i][k] = ans;
+    // }
 
 public:
-    int maxValueOfCoins(vector<vector<int>>& piles, int k) {
+    int maxValueOfCoins(vector<vector<int>>& piles, int m) {
         // dp.clear();
         // int size1 = 0;
         // for(auto i : piles){
@@ -44,10 +44,30 @@ public:
         // dp.resize(piles.size() +1, (vector<vector< int >> (size1, vector<int> (k+1, -1))));
         // return fn(0, 0, piles, k);
 
-        dp.clear();
-        dp.resize(piles.size()+1, vector< int > (k+1, -1));
 
-        return fn(0, piles, k);
+        /* 2nd method */
+        // dp.clear();
+        // dp.resize(piles.size()+1, vector< int > (k+1, -1));
 
+        // return fn(0, piles, k);
+
+        /* tabulation*/
+        int n = piles.size();
+
+        vector<vector< int >> dp(n+1, vector<int> (m+1, 0));
+
+        for(int i = n-1; i >= 0; i--){
+            for(int k = 0; k <= m; k++){
+                int ans = dp[i+1][k];
+                int sum = 0;
+                for(int j = 0; j < min(k, (int)piles[i].size()); j++){
+                    sum += piles[i][j];
+                    ans = max(ans, sum + dp[i+1][max(0,k - (j+1))]);
+                }
+                dp[i][k] = ans;
+            }
+        }
+
+        return dp[0][m];
     }
 };
