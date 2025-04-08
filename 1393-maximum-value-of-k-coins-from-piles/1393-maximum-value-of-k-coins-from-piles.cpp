eedce920cafb22c6f703f784f1c9dbd5b4435611
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-04-08 21:51:40 +0530

    Time: 156 ms (30.66%), Space: 34.3 MB (52.93%) - LeetHub

--- 1393-maximum-value-of-k-coins-from-piles/1393-maximum-value-of-k-coins-from-piles.cpp
+++ 1393-maximum-value-of-k-coins-from-piles/1393-maximum-value-of-k-coins-from-piles.cpp
@@ -1,27 +1,53 @@
 class Solution {
 private:
-    vector<vector<vector< int >>> dp;
-    int fn(int idx, int j, vector<vector< int >>&arr, int k){
-        if(k <= 0 || idx >= arr.size() || j >= arr[idx].size()){
-            return 0;
-        }
-        if(dp[idx][j][k] != -1) return dp[idx][j][k];
-        int ans = 0;
+    // vector<vector<vector< int >>> dp;
+    // int fn(int idx, int j, vector<vector< int >>&arr, int k){
+    //     if(k <= 0 || idx >= arr.size() || j >= arr[idx].size()){
+    //         return 0;
+    //     }
+    //     if(dp[idx][j][k] != -1) return dp[idx][j][k];
+    //     int ans = 0;
 
-        ans = max(ans, arr[idx][j] + fn(idx+1, 0, arr, k-1));
-        ans = max(ans, arr[idx][j] + fn(idx, j+1, arr, k-1));
-        ans = max(ans, fn(idx+1, 0, arr, k));
-        // cout<<ans<<endl;
-        return dp[idx][j][k] = ans;
+    //     ans = max(ans, arr[idx][j] + fn(idx+1, 0, arr, k-1));
+    //     ans = max(ans, arr[idx][j] + fn(idx, j+1, arr, k-1));
+    //     ans = max(ans, fn(idx+1, 0, arr, k));
+    //     // cout<<ans<<endl;
+    //     return dp[idx][j][k] = ans;
+    // }
+
+    vector<vector< int >> dp;
+
+    int fn(int i, vector<vector< int >> &arr, int k){
+        if(i >= arr.size() || k <= 0) return 0;
+
+        if(dp[i][k] != -1) return dp[i][k];
+
+        int ans = fn(i +1, arr, k);
+
+        int sum = 0;
+
+        for(int j = 0; j < min(k, (int)arr[i].size()); j++){
+            sum += arr[i][j];
+            ans = max(ans, sum + fn(i+1, arr, k - (j+1)));
+        }
+
+        return dp[i][k] = ans;
     }
+
 public:
     int maxValueOfCoins(vector<vector<int>>& piles, int k) {
+        // dp.clear();
+        // int size1 = 0;
+        // for(auto i : piles){
+        //     size1 = max({size1, (int)i.size()});
+        // }
+        // dp.resize(piles.size() +1, (vector<vector< int >> (size1, vector<int> (k+1, -1))));
+        // return fn(0, 0, piles, k);
+
         dp.clear();
-        int size1 = 0;
-        for(auto i : piles){
-            size1 = max({size1, (int)i.size()});
-        }
-        dp.resize(piles.size() +1, (vector<vector< int >> (size1, vector<int> (k+1, -1))));
-        return fn(0, 0, piles, k);
+        dp.resize(piles.size()+1, vector< int > (k+1, -1));
+
+        return fn(0, piles, k);
+
     }
 };
