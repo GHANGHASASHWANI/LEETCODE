d2e687f3111a0b7ca89b7f91e0b6e06a9b10b00f
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-04-11 19:04:18 +0530

    Time: 3 ms (94.34%), Space: 11.6 MB (73.95%) - LeetHub

--- 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.cpp
+++ 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.cpp
@@ -1,22 +1,34 @@
 class Solution {
 private:
     const int mod = 1e9 + 7;
-    vector< int > dp;
-    int fn(int len, int low, int high, int one, int zero){
-        if(len > high) return 0;
-        if(dp[len] != -1) return dp[len];
-        int ans = 0;
-        if(len >= low && len <= high) ans =  1;
+    // vector< int > dp;
+    // int fn(int len, int low, int high, int one, int zero){
+    //     if(len > high) return 0;
+    //     if(dp[len] != -1) return dp[len];
+    //     int ans = 0;
+    //     if(len >= low && len <= high) ans =  1;
 
-        int addZero = fn(len + zero, low, high, one, zero) % mod;
-        int addOne = fn(len + one, low, high, one, zero)% mod;
+    //     int addZero = fn(len + zero, low, high, one, zero) % mod;
+    //     int addOne = fn(len + one, low, high, one, zero)% mod;
 
-        return dp[len] =  ((ans + addZero) % mod + addOne) % mod;
-    }
+    //     return dp[len] =  ((ans + addZero) % mod + addOne) % mod;
+    // }
 public:
     int countGoodStrings(int low, int high, int zero, int one) {
-        dp.clear();
-        dp.resize(high + 1, -1);
-        return fn(0, low, high, zero, one);
+        // dp.clear();
+        // dp.resize(high + 1, -1);
+        // return fn(0, low, high, zero, one);
+        vector< int > dp(high + 1, 0);
+
+        for(int len = high; len >= 0; len--){
+            int ans = 0;
+            if(len >= low && len <= high) ans =  1;
+            
+            if(len + zero <= high) ans =( ans + dp[len + zero]) % mod;
+            if(len + one <= high) ans = (ans + dp[len + one]) % mod;
+
+            dp[len] =  ans;
+        }
+        return dp[0];
     }
 };
