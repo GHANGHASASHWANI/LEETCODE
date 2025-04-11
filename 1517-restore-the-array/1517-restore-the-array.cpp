fcf872c5e446065e5cc42e82fc0bbba3c613649b
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-04-11 11:47:47 +0530

    Time: 4 ms (98.89%), Space: 15.1 MB (95.58%) - LeetHub

--- 1517-restore-the-array/1517-restore-the-array.cpp
+++ 1517-restore-the-array/1517-restore-the-array.cpp
@@ -20,8 +20,29 @@ private:
     }
 public:
     int numberOfArrays(string s, int k) {
+        // dp.clear();
+        // dp.resize(s.size()+1, -1);
+        // return fn(0, s.size(),s, k);
+
         dp.clear();
-        dp.resize(s.size()+1, -1);
-        return fn(0, s.size(),s, k);
+        dp.resize(s.size()+1, 0);
+        dp[s.size()] = 1;
+        for(int i =0; i <s.size(); i++){
+            if(s[i] == '0') dp[i] = 0;
+        }
+
+        for(int j = s.size()-1; j >=0; j--){
+             if (s[j] == '0') continue; 
+             long ans = 0;
+            long long num = 0;
+            for(int i = j; i < s.size(); i++){
+                num = (num*10) + (s[i] - '0');
+                if(num > k) break;
+                // cout<<num<<endl;
+                ans += dp[i+1];
+            }
+            dp[j] = (ans)%mod;
+        }
+        return dp[0];
     }
 };
