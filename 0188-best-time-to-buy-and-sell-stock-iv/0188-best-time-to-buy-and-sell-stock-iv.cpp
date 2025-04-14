bc57a2d6c25df48a76c4d199f40402708f191995
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-04-14 20:49:52 +0530

    Time: 19 ms (19.92%), Space: 18.5 MB (17.05%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,24 +1,57 @@
 class Solution {
-public:
-    int dp[1005][1005][2];
+private:
+    int calculateProfit(int idx, int buy, int cap, vector< int> & prices, vector<vector<vector< int >>> &dp){
+        if(idx >= prices.size()) return 0;
+        if(cap <= 0) return 0;
 
-    int profit(vector<int> &prices, int i, int k, int on){
-        if(i == prices.size()) return 0;
+        if(dp[idx][buy][cap] != -1) return dp[idx][buy][cap];
 
-        if(dp[i][k][on] != -1) return dp[i][k][on];
+        int profit = 0;
+        if(buy){
+            int buyStock = calculateProfit(idx + 1, ! buy, cap, prices, dp) - prices[idx];
+            int notBuyStock = calculateProfit(idx + 1, buy, cap, prices, dp);
 
-        int res = INT_MIN;
-        res = profit(prices, i+1, k, on);
-
-        if(on){
-            res = max(res, prices[i] + profit(prices, i+1, k-1, false));
+            profit = max(buyStock, notBuyStock);
         }else{
-            if(k>0) res = max(res, profit(prices, i+1, k, true) - prices[i]);
+            int sellStock = prices[idx] + calculateProfit(idx + 1, ! buy, cap - 1, prices, dp);
+            int notSellStock = calculateProfit(idx + 1, buy, cap, prices, dp);
+            
+            profit = max(sellStock, notSellStock);
         }
-        return dp[i][k][on] = res;
+        return dp[idx][buy][cap] = profit;
     }
+public:
     int maxProfit(int k, vector<int>& prices) {
-        memset(dp, -1, sizeof dp);
-        return profit(prices, 0, k, false);
+        int len = prices.size();
+
+        vector<vector<vector< int >>> dp(len +1, vector<vector< int >> (2, vector< int> (k+1, -1)));
+
+        return calculateProfit(0, 1, k, prices, dp);
+
+    //     int len = prices.size();
+    //     vector<vector<vector< int >>> dp(len+1, vector<vector< int >> (2, vector<int> (3, 0)));
+
+    //     vector<vector< int >> curr(2, vector<int> (3, 0)), ahead(2, vector<int> (3, 0));
+
+    //     for(int i = len - 1; i >= 0; i--){
+    //         for(int j = 0; j <= 1; j++){
+    //             for(int k = 1; k <= 2; k++){
+    //                 int profit = 0;
+    //                 if(j){
+    //                     int buyStock = ahead[!j][k] - prices[i];
+    //                     int notBuyStock = ahead[j][k];
+    //                     profit = max(buyStock, notBuyStock);
+    //                 }else{
+    //                     int sellStock = ahead[!j][k - 1] + prices[i];
+    //                     int notSellStock = ahead[j][k];
+    //                     profit = max(sellStock, notSellStock);
+    //                 }
+    //                 curr[j][k] = profit;
+    //             }
+    //             ahead = curr;
+    //         }
+    //     }
+
+    //     return curr[1][2];
     }
 };
