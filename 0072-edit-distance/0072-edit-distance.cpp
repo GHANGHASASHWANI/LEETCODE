36884a4af662dd00620e3cc8e6125a90b5052330
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-04-14 14:05:27 +0530

    Time: 7 ms (60.64%), Space: 13.5 MB (23.81%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,28 +1,51 @@
 class Solution {
-public:
-    int dp[505][505];
-    int solve(string s1, string s2, int i, int j) {
-        if (i == s1.size())
-            return s2.size() - j;
-        if (j == s2.size())
-            return s1.size() - i;
+private:
+    int findDistance(int idx1, const string &word1, int idx2, const string &word2, vector<vector< int >> &dp){
+        if(idx1 == 0 ) return idx2;
+        if(idx2 == 0) return idx1;
 
-        if (dp[i][j] != -1)
-            return dp[i][j];
+        if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
 
-        int res = 1000000;
-
-        if (s1[i] == s2[j]) {
-            res = min(res, solve(s1, s2, i + 1, j + 1));
-        } else {
-            res = min(res, 1 + solve(s1, s2, i + 1, j + 1));
-            res = min(res, 1 + solve(s1, s2, i + 1, j));
-            res = min(res, 1 + solve(s1, s2, i, j + 1));
+        if(word1[idx1 - 1] == word2[idx2 - 1]){
+            return findDistance(idx1 - 1, word1, idx2 - 1, word2, dp);
         }
-        return dp[i][j] = res;
+
+        int insertVal = 1 + findDistance(idx1, word1, idx2 -  1, word2, dp);
+        int deleteVal = 1 + findDistance(idx1 - 1, word1, idx2, word2, dp);
+        int replaceVal = 1 + findDistance(idx1 - 1, word1, idx2 -  1, word2, dp);
+
+        return min({insertVal, deleteVal, replaceVal});
     }
+public:
     int minDistance(string word1, string word2) {
-        memset(dp, -1, sizeof dp);
-        return solve(word1, word2, 0, 0);
+        // int len1 = word1.size(), len2 = word2.size();
+        // vector<vector< int >> dp(len1 + 1, vector< int > (len2 + 1, -1));
+        // return findDistance(len1, word1, len2, word2, dp);
+
+        int len1 = word1.size(), len2 = word2.size();
+
+        vector<vector< int >> dp(len1 + 1, vector< int > (len2 + 1, 0));
+
+        for(int i =0; i <= len1; i++){
+            dp[i][0] = i;
+        }
+        for(int j =0; j <= len2; j++){
+            dp[0][j] = j;
+        }
+
+        for(int i =1; i <= len1; i++){
+            for(int j = 1; j <= len2; j++){
+                if(word1[i - 1] == word2[j - 1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    int replaceVal = 1 + dp[i - 1][j - 1];
+                    int deleteVal = 1 + dp[i - 1][j];
+                    int insertVal = 1 + dp[i][j - 1];
+
+                    dp[i][j] = min({replaceVal, deleteVal, insertVal});
+                }
+            }
+        }
+        return dp[len1][len2];
     }
 };
