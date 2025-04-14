6e1f393814a50aa6e48b840b920a64d3d5b4d484
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-04-14 13:24:31 +0530

    Time: 17 ms (82.59%), Space: 44.5 MB (13.67%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -32,14 +32,14 @@ public:
         // }
 
         for(int i = 1; i <= len1; i++){
-            for(int j = 1; j <= len2; j++){
+            for(int j = len2; j >= 1; j--){
                 if(s[i - 1] == t[j - 1]){
-                   curr[j] = prev[j-1] + prev[j];
+                   prev[j] = prev[j-1] + prev[j];
                 }else{
-                    curr[j] = prev[j];
+                    prev[j] = prev[j];
                 }   
             }
-            prev = curr;
+            // prev = curr;
         }
 
         return (int)prev[len2];
