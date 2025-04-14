88c47a667998bb564ba1324ea80bbd345063b17c
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-04-14 10:06:24 +0530

    Time: 26 ms (63.18%), Space: 29.3 MB (6.71%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -10,6 +10,20 @@ public:
                 else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
             }
         }
+        string res = "";
+        int i =n, j = m;
+        while(i && j){
+            if(dp[i-1][j] == dp[i][j-1]){
+                res = text1[i-1] + res;
+                i--;
+                j--;
+            }else if(dp[i-1][j] > dp[i][j-1]){
+                i = i-1;
+            }else{
+                j = j-1;
+            }
+        }
+        cout<<res<<endl;
         return dp[n][m];
     }
 };
