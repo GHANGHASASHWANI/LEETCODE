724e0611e2f4f7809144d8cdacda1737535ca3fa
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-04-14 16:51:26 +0530

    Time: 35 ms (44.84%), Space: 15 MB (52.06%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -30,8 +30,43 @@ private:
     }
 public:
     bool isMatch(string s, string p) {
+        // int len1 = s.size(), len2 = p.size();
+        // vector<vector< int >> dp(len1 + 1, vector< int > (len2 + 1, -1));
+        // return checkString(s, len1, p, len2, dp);
+
         int len1 = s.size(), len2 = p.size();
-        vector<vector< int >> dp(len1 + 1, vector< int > (len2 + 1, -1));
-        return checkString(s, len1, p, len2, dp);
+        vector<vector< bool >> dp(len1 + 1, vector< bool > (len2 + 1, false));
+        dp[0][0] = true;
+
+        for(int i =1; i <= len1; i++){
+            dp[i][0] = false;
+        }
+        for(int i =0; i <= len2; i++){
+            int flag = true;
+            for(int j = 1; j <= i; j++){
+                if(p[j -1 ] != '*'){
+                    flag = false;
+                    break;
+                }
+            }
+           dp[0][i] = flag;
+        }
+
+        for(int i = 1; i <= len1; i++){
+            for(int j = 1; j <= len2; j++){
+                bool isSame = false;
+                if(s[i-1] == p[j - 1]){
+                    isSame = dp[i - 1][j - 1];
+                }
+                else if(p[j - 1] == '?'){
+                    isSame = dp[i - 1][j - 1];
+                }
+                else if(p[j - 1] == '*'){
+                    isSame = dp[i - 1][j] || dp[i][j - 1];
+                }
+                dp[i][j] = isSame;
+            }
+        }
+        return dp[len1][len2];
     }
 };
