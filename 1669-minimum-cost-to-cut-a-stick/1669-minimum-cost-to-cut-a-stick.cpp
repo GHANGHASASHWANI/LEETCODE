97a6436c22d563ac7fde3d2380a343f21861d8ae
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-04-15 17:21:38 +0530

    Time: 19 ms (82.92%), Space: 14 MB (47.45%) - LeetHub

--- 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
@@ -1,26 +1,57 @@
 class Solution {
-public:
-    vector<vector<int>> dp;
-    int minimumCost(int i, int j, vector<int>& cuts) {
-        if (i > j)
-            return 0;
-        if (dp[i][j] != -1)
-            return dp[i][j];
-        int mini = INT_MAX;
-        for (int k = i; k <= j; k++) {
-            int steps = cuts[j + 1] - cuts[i - 1] +
-                        minimumCost(i, k - 1, cuts) +
-                        minimumCost(k + 1, j, cuts);
-            mini = min(mini, steps);
+private:
+    int findCuts(int start, int end, const vector< int> &cuts, vector<vector< int >> & dp){
+        if(start > end) return 0;
+
+        if(dp[start][end] != -1) return dp[start][end];
+
+        long long minCut = INT_MAX;
+
+        for(int idx = start; idx <= end; idx++){
+            long long partition1 = findCuts(start, idx - 1, cuts, dp);
+            long long partition2 = findCuts(idx + 1, end, cuts, dp);
+            long long length = cuts[end + 1] - cuts[start - 1];
+
+            minCut = min(minCut, partition1 + partition2 + length);
         }
-        return dp[i][j] = mini;
+        return dp[start][end] = minCut;
     }
+public:
     int minCost(int n, vector<int>& cuts) {
-        dp.clear();
-        cuts.insert(cuts.begin(), 0);
+        // cuts.push_back(n);
+        // cuts.insert(cuts.begin(), 0);
+
+        // sort(cuts.begin(), cuts.end());
+
+        // int N = cuts.size();
+
+        // vector<vector< int >> dp(N, vector< int > (N, -1));
+
+        // return findCuts(1, N-2, cuts, dp);
+
         cuts.push_back(n);
+        cuts.insert(cuts.begin(), 0);
+
         sort(cuts.begin(), cuts.end());
-        dp.resize(n + 5, vector<int>(n + 5, -1));
-        return minimumCost(1, cuts.size() - 2, cuts);
+
+        int N = cuts.size();
+
+        vector<vector< int >> dp(N, vector<int> (N, 0));
+
+        for(int i = N - 2;  i >= 1; i--){
+            for(int j = 1; j <= N - 2; j++){
+                if(i > j) continue;
+                int miniCost = INT_MAX;
+                for(int k = i; k <= j; k++){
+                    int partition1 = dp[i][k-1];
+                    int partition2 = dp[k+1][j];
+                    int cost = cuts[j + 1] - cuts[i - 1];
+
+                    miniCost = min(miniCost, partition1 + partition2 + cost);
+                }
+                dp[i][j] = miniCost;
+            }
+        }
+        return dp[1][N-2];
     }
 };
