842017d0783ff212108af0302266096305c52b94
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-04-15 18:05:51 +0530

    Time: 94 ms (78.96%), Space: 13.9 MB (28.02%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -18,10 +18,34 @@ private:
     }
 public:
     int maxCoins(vector<int>& nums) {
+        // nums.push_back(1);
+        // nums.insert(nums.begin(), 1);
+        // int n = nums.size();
+        // vector<vector< int >> dp(n, vector< int >(n, -1));
+        // return brust(1, n - 2, nums, dp);
+
         nums.push_back(1);
         nums.insert(nums.begin(), 1);
         int n = nums.size();
-        vector<vector< int >> dp(n, vector< int >(n, -1));
-        return brust(1, n - 2, nums, dp);
+        vector<vector< int >> dp(n, vector< int >(n, 0));
+
+        for(int start = n - 2; start >= 1; start--){
+            for(int end = 1; end <= n - 2; end++){
+                if(start > end) continue;
+
+                int maxBrust = INT_MIN;
+
+                for(int idx = start; idx <= end; idx++){
+                    int partition1 = dp[start][idx-1];
+                    int partition2 = dp[idx+1][end];
+                    int score = nums[start - 1] * nums[idx]* nums[end + 1];
+
+                    maxBrust = max(maxBrust, score + partition1 + partition2);
+                }
+                dp[start][end] = maxBrust;
+            }
+        }
+
+        return dp[1][n - 2];
     }
 };
