572acb1acc27f998c7caabaa81a63a7140e75872
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-04-15 09:45:13 +0530

    Time: 0 ms (100%), Space: 13.9 MB (94.48%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -19,22 +19,67 @@ public:
         // vector<vector< int >> dp(len, vector<int> (len + 1, -1));
         // return longestLIS(0, -1, nums, dp);
 
-        const int len = nums.size();
-        vector<vector< int >> dp(len + 1, vector< int > (len + 2, 0));
+        // const int len = nums.size();
+        // vector<vector< int >> dp(len + 1, vector< int > (len + 2, 0));
 
-        vector< int > curr(len + 2, 0), ahead(len + 2, 0);
+        // vector< int > curr(len + 2, 0), ahead(len + 2, 0);
 
-        for(int i = len - 1; i >= 0; i--){
-            for(int j = i - 1; j >= -1; j--){
-                int notTake = ahead[j+1];
-                int take = 0;
-                if(j == -1 || nums[i] > nums[j]){
-                    take = 1 + ahead[i+1];
-                }
-                curr[j + 1] = max(take, notTake);
+        // for(int i = len - 1; i >= 0; i--){
+        //     for(int j = i - 1; j >= -1; j--){
+        //         int notTake = ahead[j+1];
+        //         int take = 0;
+        //         if(j == -1 || nums[i] > nums[j]){
+        //             take = 1 + ahead[i+1];
+        //         }
+        //         curr[j + 1] = max(take, notTake);
+        //     }
+        //     ahead = curr;
+        // }
+        // return ahead[0];
+        // int n = nums.size();
+
+        // vector< int > dp(n, 1);
+        // vector< int > position(n);
+        // int lastIdx = 0;
+        // int result = 0;
+        // for(int i =0; i < n; i++){
+        //     position[i] = i;
+        //     for(int j = 0; j < i; j++){
+        //         if(nums[i] > nums[j] && dp[i] < 1 + dp[j]){
+        //             dp[i] = 1 + dp[j];
+        //             position[i] = j;
+        //         }
+        //     }
+        //     if(dp[i] > result){
+        //         result = dp[i];
+        //         lastIdx = i;
+        //     }
+        // }
+
+        // string ans = "";
+
+        // while(position[lastIdx] != lastIdx){
+        //     ans = to_string(nums[lastIdx]) + ans;
+        //     lastIdx = position[lastIdx];
+        // }
+        // cout<<ans<<endl;
+
+        // return result;
+
+        int len = nums.size();
+        vector< int > res;
+        res.push_back(nums[0]);
+
+        for(int i = 0; i < nums.size(); i++){
+            int idx = lower_bound(res.begin(), res.end(), nums[i]) - res.begin();
+
+            if(idx == res.size()){
+                res.push_back(nums[i]);
+            }else{
+                res[idx] = nums[i];
             }
-            ahead = curr;
         }
-        return ahead[0];
+
+        return res.size();
     }
 };
