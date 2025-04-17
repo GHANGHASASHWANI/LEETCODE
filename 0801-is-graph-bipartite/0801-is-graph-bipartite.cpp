d900670495476b8fba0c087ec06b815c40aed9ec
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-04-17 18:26:06 +0530

    Time: 0 ms (100%), Space: 17.3 MB (60.33%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,6 +1,6 @@
 class Solution {
 private:
-    bool checkGraph(const int node, const vector<vector< int >>& graph, vector< int > &color){
+    bool checkGraphBFS(const int node, const vector<vector< int >>& graph, vector< int > &color){
         queue< int > queueBfs;
         queueBfs.push(node);
         color[node] = 0;
@@ -22,6 +22,22 @@ private:
         return true;
     }
 
+    bool checkGraphDFS(const int node, const vector<vector< int>>& graph, vector< int>& color){
+
+        for(auto& newNode : graph[node]){
+            if(color[newNode] == -1){
+                color[newNode] = not color[node];
+                if(not checkGraphDFS(newNode, graph, color)){
+                    return false;
+                }
+            }
+            else if(color[newNode] == color[node]){
+                return false;
+            }
+        }
+        return true;
+    }
+
 public:
     bool isBipartite(vector<vector<int>>& graph) {
         const int totalVertex = graph.size();
@@ -29,9 +45,12 @@ public:
         vector< int > color(totalVertex, -1);
 
         for(int i =0; i < totalVertex; i++){
-            if(color[i] == -1 && not checkGraph(i, graph, color)){
-                return false;
-            }
+            if(color[i] == -1){
+                color[i] = 0;
+                if(not checkGraphDFS(i, graph, color)){
+                    return false;
+                }
+            } 
         }
         return true;
     }
