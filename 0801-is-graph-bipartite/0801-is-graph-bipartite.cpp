a6e33dc63195d3e77dda412bc27b1192be8a456c
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-04-17 18:20:40 +0530

    Time: 3 ms (16.04%), Space: 17.6 MB (17.75%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,54 +1,38 @@
 class Solution {
-public:
+private:
+    bool checkGraph(const int node, const vector<vector< int >>& graph, vector< int > &color){
+        queue< int > queueBfs;
+        queueBfs.push(node);
+        color[node] = 0;
 
-    // bool checkGraph(vector<vector<int>>& graph, vector<int> &color, int node){
-    //     queue<int> qu;
-    //     qu.push(node);
-    //     color[node] = 0;
-    //     while(qu.size()){
-    //         int temp = qu.front();
-    //         qu.pop();
-    //         for(auto it: graph[temp]){
-    //             if(color[it] == -1){
-    //                 color[it] = !color[temp];
-    //                 qu.push(it);
-    //             }
-    //             else if (color[it] == color[temp]){
-    //                 return false;
-    //             }
-    //         }
-    //     }
-    //     return true;
-    // }
+        while(not queueBfs.empty()){
+            int currNode = queueBfs.front();
+            queueBfs.pop();
 
-    bool checkGraph(vector<vector< int >> &graph, vector< int > &color, int current, int previousColor){
-
-        for(auto node : graph[current]){
-            if(color[node] == -1){
-                color[node] = not color[current];
-                // cout<<"color seq "<<node << " " << color[node]<< endl;
-                if( not checkGraph(graph, color, node, color[node])) return false;
-            }
-            else if(color[node] == color[current]){
-                // cout<<" stopped "<< node <<" "<< current<<" " << color[node] << endl;
-                return false;
+            for(auto& newNode : graph[currNode]){
+                if(color[newNode] == -1){
+                    color[newNode] = not color[currNode];
+                    queueBfs.push(newNode);
+                }
+                else if(color[newNode] == color[currNode]){
+                    return false;
+                }
             }
         }
         return true;
     }
 
-
+public:
     bool isBipartite(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int> color(n, -1);
-        
-        for(int i=0;i<n;i++){
-            if(color[i] == -1){
-                color[i] = 0;
-                bool ans = checkGraph(graph,color,i, 0);
-                if(!ans) return false;
+        const int totalVertex = graph.size();
+
+        vector< int > color(totalVertex, -1);
+
+        for(int i =0; i < totalVertex; i++){
+            if(color[i] == -1 && not checkGraph(i, graph, color)){
+                return false;
             }
         }
-    return true;
+        return true;
     }
 };
