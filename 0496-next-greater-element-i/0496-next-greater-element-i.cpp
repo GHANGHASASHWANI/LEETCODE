e6268a89e0be89080d529a9838f8bff87d4f4599
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-05-12 20:22:06 +0530

    Time: 1 ms (56.39%), Space: 13 MB (13.45%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector< int > nextElement;
-        map< int, int> mpElement;
-        stack< int > st;
-        for(int i = 0; i < nums2.size(); i++){
-            while(!st.empty() && st.top()<nums2[i]){
-                int top = st.top();
+        map<int, int> nge;
+        stack<int> st;
+
+        for(int i = nums2.size() - 1; i >= 0; i--){
+            while (!st.empty() && st.top() <= nums2[i]) {
                 st.pop();
-                mpElement[top] = nums2[i];
             }
+            if (st.empty()) nge[nums2[i]] = -1;
+            else nge[nums2[i]] = st.top();
+
             st.push(nums2[i]);
         }
+        vector<int> answer(nums1.size());
 
-        for(int i = 0; i < nums1.size(); i++){
-            if(mpElement.find(nums1[i]) == mpElement.end()){
-                nextElement.push_back(-1);
-            }else{
-                nextElement.push_back(mpElement[nums1[i]]);
+        for (int i = 0; i < nums1.size(); i++){
+            if(nge.find(nums1[i]) == nge.end()){
+                answer[i] = -1;
+            }
+            else {
+                answer[i] = nge[nums1[i]];
             }
         }
-        return nextElement;
+
+        return answer;
     }
 };
