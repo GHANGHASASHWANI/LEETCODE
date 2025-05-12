fd80432538eddfcce106d8c8445990338c84b97f
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-05-12 21:25:30 +0530

    Time: 15 ms (70.08%), Space: 47.1 MB (61.72%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
@@ -1,43 +1,58 @@
 class Solution {
 public:
-    vector< int > ns(vector< int > & arr){
+    vector<int> nse(vector<int> &arr){
+        vector<int> ans(arr.size());
         int n = arr.size();
-        vector< int> nsee(n);
         stack<int> st;
-
-        for(int i =n-1; i >=0; i--){
-            while(!st.empty() && arr[st.top()] >= arr[i]){
+        for(int i = n - 1; i >= 0; i--){
+            while (!st.empty() && arr[st.top()] > arr[i]) {
                 st.pop();
             }
-            nsee[i] = st.empty() ? n : st.top();
+            if (!st.empty()) {
+                ans[i] = st.top();
+            }
+            else {
+                ans[i] = n;
+            }
             st.push(i);
         }
-        return nsee;
+        return ans;
     }
 
-    vector< int > nge(vector< int > &arr){
+    vector<int> pse(vector<int> &arr){
+        vector<int> ans(arr.size());
         int n = arr.size();
-        vector< int > ngee(n);
-        stack< int > st;
-        for(int i =0; i < arr.size(); i++){
-            while(!st.empty() && arr[st.top()] > arr[i]){
+        stack<int> st;
+
+        for (int i = 0; i < n; i++){
+            while (!st.empty() && arr[st.top()] >= arr[i]){
                 st.pop();
             }
-            ngee[i] = st.empty() ? -1 : st.top();
+            if (!st.empty()) {
+                ans[i] = st.top();
+            }
+            else {
+                ans[i] = -1;
+            }
             st.push(i);
         }
-        return ngee;
+        return ans;
     }
 
     int sumSubarrayMins(vector<int>& arr) {
-       vector< int > nse = ns(arr);
-       vector< int > psee = nge(arr);
-       int total = 0, mod = (int)(1e9+7);
-       for(int i =0; i < arr.size(); i++){
-        int left = i - psee[i];
-        int right = nse[i] - i;
-        total = (total + (right * left * 1LL*arr[i])%mod)%mod;
-       }
-       return total;
+        vector<int> psee = pse(arr);
+        vector<int> nsee = nse(arr);
+
+        int total = 0, mod = 1e9 + 7;
+
+        for (int i =0; i < arr.size(); i++) {
+            int leftArr = i - psee[i];
+            int rightArr = nsee[i] - i;
+
+            // cout<<leftArr<<" "<<rightArr<<endl;
+
+            total = (total + 1LL * leftArr * rightArr * arr[i]) % mod;
+        }
+        return total;
     }
 };
