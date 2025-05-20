d00daa6d8b8b6e6a63664add9c6c14afb488c4a1
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-05-20 16:30:26 +0530

    Time: 4 ms (83.72%), Space: 292.7 MB (96.48%) - LeetHub

---
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        int n = nums.size();
+        vector<int> arr(n, 0);
+
+        for (int i= 0; i < queries.size(); i++) {
+            int start = queries[i][0];
+            int end = queries[i][1];
+            if (end < n - 1) {
+                arr[start] += -1;
+                arr[end + 1] += 1;
+            }
+            else {
+                arr[start] += -1;
+            }
+        }
+
+        for (int i = 1; i < n; i++) {
+            arr[i] = arr[i] + arr[i - 1];
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] + nums[i] > 0) return false;
+        }
+
+        return true;
+    }
+};
