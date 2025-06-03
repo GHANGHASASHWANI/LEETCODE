379dae9fb3e73b6027cd98a746b30c15f5e58e2c
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-06-03 18:49:42 +0530

    Time: 0 ms (100%), Space: 44.5 MB (50.48%) - LeetHub

--- 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.cpp
+++ 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.cpp
@@ -1,40 +1,44 @@
 class Solution {
 public:
     int maxCandies(vector<int>& status, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, vector<int>& initialBoxes) {
-        int total = 0;
-        vector<bool> visited(status.size(), false);
-        vector<bool> hasBox(status.size(), false);
-        queue<int> qu;
+        int n = status.size();
+        vector<int> hasBox(n, false);
+        vector<int> visited(n, false);
 
-        for (auto i : initialBoxes){ 
-            hasBox[i] = true;
-            if (status[i]) {
-                visited[i] = true;
-                qu.push(i);
+        queue<int> qu;
+        int total = 0;
+
+        for (auto it: initialBoxes) {
+            hasBox[it] = true;
+
+            if (status[it]) {
+                qu.push(it);
+                visited[it] = true;
             }
         }
 
-        while (!qu.empty()) {
+        while (not qu.empty()) {
             int top = qu.front();
             qu.pop();
-
             total += candies[top];
 
-            for (auto it : keys[top]) {
-                if (!status[it]) {
-                    status[it] = 1;
-                    if (hasBox[it] && !visited[it]) {
+            for (auto &it : keys[top]) {
+                if (not status[it]) {
+                    status[it] = true;
+                    if (hasBox[it] and not visited[it]) {
                         visited[it] = true;
                         qu.push(it);
                     }
                 }
             }
 
-            for (auto it : containedBoxes[top]) {
+            for (auto &it : containedBoxes[top]) {
                 hasBox[it] = true;
-                if (status[it] && !visited[it]) {
-                    visited[it] = true;
-                    qu.push(it);
+                if (hasBox[it]) {
+                    if (status[it] and not visited[it]) {
+                        visited[it] = true;
+                        qu.push(it);
+                    }
                 }
             }
         }
