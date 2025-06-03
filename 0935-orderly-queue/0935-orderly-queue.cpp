9cbb3cafe6a9e8edb2861c813c16c243cd223485
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-06-03 14:38:16 +0530

    Time: 0 ms (100%), Space: 8.4 MB (98.25%) - LeetHub

--- 0935-orderly-queue/0935-orderly-queue.cpp
+++ 0935-orderly-queue/0935-orderly-queue.cpp
@@ -2,11 +2,14 @@ class Solution {
 public:
     string orderlyQueue(string s, int k) {
         if (k == 1) {
-           string minStr = s;
-           for (int i = 0; i < s.size(); i++) {
-            minStr = min(minStr, s.substr(i) + s.substr(0, i));
-           }
-           return minStr;
+            string minStr = s;
+          for (int i = 0; i < s.size(); i++) {
+            char st = s[0];
+            s.erase(0, 1);
+            s.push_back(st);
+            minStr = min(minStr, s);
+          }
+          return minStr;
         }
         sort(s.begin(), s.end());
         return s;
