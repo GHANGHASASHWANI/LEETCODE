04ee41d45837cdc838d5a22c9dbad406847c3a17
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-06-09 18:11:42 +0530

    Time: 0 ms (100%), Space: 7.8 MB (84.44%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,14 +1,8 @@
 class Solution {
 public:
-    bool check(int n){
-        
-        if(n == 1 ) return true;
-        if(n % 2 != 0) return false;
-
-        return check(n / 2);
-    }
     bool isPowerOfTwo(int n) {
-        if(n == 0) return false;
-       return check(n);
+        if (n == 0) return false;
+        if (n > 0 && ((n & (n - 1)) ==0 )) return true;
+        return false;
     }
 };
