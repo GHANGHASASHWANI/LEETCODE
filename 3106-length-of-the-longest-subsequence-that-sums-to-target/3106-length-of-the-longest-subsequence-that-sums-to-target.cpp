e835c56b4d06b0d2d89eeff5e6950f004bfb6779
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-06-11 15:04:40 +0530

    Time: 386 ms (9.17%), Space: 157.9 MB (33.26%) - LeetHub

--- 3106-length-of-the-longest-subsequence-that-sums-to-target/3106-length-of-the-longest-subsequence-that-sums-to-target.cpp
+++ 3106-length-of-the-longest-subsequence-that-sums-to-target/3106-length-of-the-longest-subsequence-that-sums-to-target.cpp
@@ -1,17 +1,26 @@
 class Solution {
+private:
+    int fn(int idx, int sum, int target, vector<int> &nums, vector<vector<int>> &dp) {
+        if (sum == target) return 0;
+        if (idx >= nums.size() || (target - sum) < 0) return sum == target ? 0 : INT_MIN;
+        
+
+        if(dp[idx][sum] != -1) return dp[idx][sum];
+
+        int pick =  1 + fn(idx + 1, sum + nums[idx], target, nums, dp);
+        int nonPick = fn(idx + 1, sum, target, nums, dp);
+
+        return dp[idx][sum] = max(pick, nonPick);
+    }
 public:
     int lengthOfLongestSubsequence(vector<int>& nums, int target) {
-        vector<int> dp(target + 1, INT_MIN);
-        dp[0] = 0;
+        int n = nums.size();
+        // int total = accumulate(nums.begin(), nums.end(), 0);
+        
+        vector<vector<int>> dp(n+1, vector<int> (target + 1, -1));
 
-        for (int num : nums) {
-            for (int j = target; j >= num; --j) {
-                if (dp[j - num] != INT_MIN) {
-                    dp[j] = max(dp[j], 1 + dp[j - num]);
-                }
-            }
-        }
-
-        return dp[target] == INT_MIN ? -1 : dp[target];
+        int ans = fn(0, 0, target, nums, dp);
+        if (ans < -1) return -1;
+        return ans;
     }
 };
