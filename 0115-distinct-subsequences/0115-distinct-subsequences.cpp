29abd3e8b805cbaf4f4c3cc1c265c80cc0b9cbbf
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-06-12 13:52:45 +0530

    Time: 36 ms (27.17%), Space: 27.1 MB (53.05%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,48 +1,26 @@
 class Solution {
 private:
-    // Count the total distinct ways 
-    int countDistinct(const string &s,const  string &t, int idx1, int idx2, vector<vector<int>> &dp){
-        if(idx2 == 0) return 1;
-        if(idx1 == 0 || idx2 == 0) return 0;
-        if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
+    vector<vector<int>> dp;
+    int fn(int i, int j, string &s, string &t) {
+        if (j < 0) return 1;
+        if (i == 0 && j == 0) return s[i] == t[j] ? 1 : 0;
+        if (i == 0) return 0;
 
-        int countIdx = 0, notCountIdx = 0;
-        // If the equal values are encountered
-        if(s[idx1 - 1] == t[idx2 - 1]){
-            countIdx = countDistinct(s, t, idx1 - 1, idx2 - 1, dp);
-        }
-        // If the equal values are not encountered
-        notCountIdx = countDistinct(s, t, idx1 - 1, idx2, dp);
+        if (dp[i][j] != -1) return dp[i][j];
 
-        return dp[idx1][idx2] = countIdx + notCountIdx;
+        int ans1 = 0, ans2 = 0;
+        if (s[i] == t[j]) {
+            ans1 = fn(i - 1, j - 1, s, t);
+        } 
+        ans2 = fn(i - 1, j, s, t);
+        
+        return dp[i][j] = ans1 + ans2;
     }
 public:
     int numDistinct(string s, string t) {
-        int len1 = s.size(), len2 = t.size();
-        vector<vector< double >> dp(len1 + 1, vector<double> (len2 + 1, 0));
+        int n = s.size(), m = t.size();
+        dp.resize(n, vector<int> (m, -1));
 
-        // return countDistinct(s, t, len1 , len2, dp);
-
-        vector< double > prev(len2 +1, 0), curr(len2 + 1, 0);
-
-        prev[0] = curr[0] = 1;
-
-        // for(int i = 0; i < len1; i++){
-        //     dp[i][0] = 1;
-        // }
-
-        for(int i = 1; i <= len1; i++){
-            for(int j = len2; j >= 1; j--){
-                if(s[i - 1] == t[j - 1]){
-                   prev[j] = prev[j-1] + prev[j];
-                }else{
-                    prev[j] = prev[j];
-                }   
-            }
-            // prev = curr;
-        }
-
-        return (int)prev[len2];
-
+        return fn(n - 1, m - 1, s, t);
     }
 };
