1d802b72091663ea9e8bee825cdffa9f3377e1d8
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-06-13 13:54:30 +0530

    Time: 33 ms (57.22%), Space: 31.4 MB (70.79%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,41 +1,25 @@
 class Solution {
 private:
     vector<vector<int>> dp;
-    int fn(int i, int j, string &str){
-        if(i >= j) return 0;
+    int fn(int i, int j, string &s) {
+        if (i > j) return 0;
 
-        if(dp[i][j] != -1) return dp[i][j];
+        if (dp[i][j] != -1) return dp[i][j];
 
-        int ans1 = 0, ans2 = 0;
+        int equal = 1e5, notEqual = 1e5;
 
-        if(str[i] == str[j]){
-            ans1 = fn(i+1, j-1, str);
-        }else{
-            ans1 = 1 + min(fn(i+1, j, str), fn(i, j-1, str));
+        if (s[i] == s[j]) { 
+            equal = fn(i + 1, j - 1, s);
         }
-        return dp[i][j] = ans1;
+        else notEqual = 1 +  min(fn(i + 1, j, s), fn(i, j - 1, s));
+
+        return dp[i][j] = min(equal, notEqual);
     }
 public:
     int minInsertions(string s) {
         int n = s.size();
-        // dp.clear();
-        // dp.resize(n, vector<int> (n, -1));
-        // return fn(0, n-1, s);
-
         dp.clear();
-        dp.resize(n+1, vector<int> (n+1, 0));
-
-        for(int i =n-2; i >= 0; i--){
-            for(int j = i+1; j < n; j++){
-                int ans1 = 0;
-                if(s[i] == s[j]){
-                    ans1 = dp[i+1][j-1];
-                }else{
-                    ans1 = 1 + min(dp[i+1][j], dp[i][j-1]);
-                }
-                dp[i][j] = ans1;
-            }
-        }
-        return dp[0][n-1];
+        dp.resize(n, vector<int> (n, -1));
+        return fn(0, s.size() - 1, s);
     }
 };
