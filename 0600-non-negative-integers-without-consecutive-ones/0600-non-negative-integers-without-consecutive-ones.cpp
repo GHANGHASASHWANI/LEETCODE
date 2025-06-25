4d5e4daeed977a422095cc4ce4b077b15d4b8056
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-06-25 18:16:38 +0530

    Time: 0 ms (100%), Space: 8.4 MB (64.84%) - LeetHub

--- 0600-non-negative-integers-without-consecutive-ones/0600-non-negative-integers-without-consecutive-ones.cpp
+++ 0600-non-negative-integers-without-consecutive-ones/0600-non-negative-integers-without-consecutive-ones.cpp
@@ -1,30 +1,36 @@
 class Solution {
 private:
-    string str;
-    int dp[30][2][2];
-    int fn(int idx, bool tight, bool prev){
-        if(idx == str.size()) return 1;
-        if(dp[idx][tight][prev] != -1) return dp[idx][tight][prev];
+    int dp[32][2][2];
+    int fn (int i, int tight, int prev, string &str) {
+        if (i == str.size()) return 1;
 
-        int ans = 0, limit = (tight ? str[idx] - '0' : 1);
+        if (dp[i][tight][prev] != -1) return dp[i][tight][prev];
 
-        for(int i =0; i <= limit; i++){
-            if(i == 1 && prev) return ans;
-            else{
-                ans += fn(idx+1, tight && (i == str[idx] - '0'), i == 1);
+        int limit = tight ? str[i] - '0' : 1;
+        int ans  = 0;
+        for (int d =0; d <= limit; d++) {
+            int newTight = tight & (d == limit);
+
+            if (prev && d == 1) {
+                return ans;
+            } else {
+                ans  += fn(i + 1, newTight, d == 1, str);
             }
         }
-        return dp[idx][tight][prev] = ans;
-    }
+        return dp[i][tight][prev] = ans;
+    } 
 public:
     int findIntegers(int n) {
-        memset(dp, -1, sizeof(dp));
-        while(n > 0){
+        string str = "";
+        while (n > 0) {
             int rem = n % 2;
             n /= 2;
-            str.push_back((char)rem + '0');
+            str += (rem + '0');
         }
         reverse(str.begin(), str.end());
-        return fn(0, 1, 0);
+
+        memset(dp, -1, sizeof(dp));
+        
+        return fn(0, true, 0, str);
     }
 };
