75b6b060db500b3c8dbdaa2449f2b4ea8dac3df9
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-06-30 19:31:20 +0530

    Time: 0 ms (100%), Space: 14.6 MB (44.33%) - LeetHub

--- 0871-keys-and-rooms/0871-keys-and-rooms.cpp
+++ 0871-keys-and-rooms/0871-keys-and-rooms.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
-    void visitRooms(vector<vector<int>>& rooms, int currentRoom,
-                    vector<int>& visited) {
-        visited[currentRoom] = 1;
-        for (auto neighbour : rooms[currentRoom]) {
-            if (not visited[neighbour]) {
-                visitRooms(rooms, neighbour, visited);
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        int n = rooms.size();
+        vector<bool> visited(n, false);
+
+        queue<int> keys;
+        keys.push(0);
+        visited[0] = true;
+        
+        while (not keys.empty()) {
+            int currRoom = keys.front();
+            keys.pop();
+
+            for (auto key : rooms[currRoom]) {
+                if (not visited[key]) {
+                    keys.push(key);
+                    visited[key] = true;
+                }
             }
         }
-    }
-    bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        int totalRooms = rooms.size();
-        vector<int> visited(totalRooms, 0);
-        visitRooms(rooms, 0, visited);
-
-        for (int i = 0; i < visited.size(); i++) {
-            if (not visited[i])
-                return false;
+        for (int i = 0; i < n; i++) {
+            if (not visited[i]) return false;
         }
         return true;
     }
