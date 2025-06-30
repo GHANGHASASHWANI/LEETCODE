72b54a31f987116b150ec3db71f679a5dca3e84c
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-06-30 17:59:08 +0530

    Time: 0 ms (100%), Space: 8.9 MB (58.2%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
@@ -1,25 +1,41 @@
 class Solution {
 public:
-  int minMutation(string start, string end, vector<string>& b) {
-    map<string,int> dist;
-    dist[start] = 0;
-    queue<string> q;
-    q.push(start);
-    while (q.size()) {
-      auto u = q.front(); q.pop();
-      for (auto v : b) {
-        if (dist.count(v)) continue;
-        int cnt = 0;
-        for (int i = 0; i < 8; i++) {
-          if (u[i] != v[i]) cnt++;
+    typedef pair<int, string> pp;
+    int minMutation(string startGene, string endGene, vector<string>& bank) {
+        priority_queue<pp, vector<pp>, greater<pp>> genes;
+
+        set<string> allGenes(bank.begin(), bank.end());
+
+        vector<char> changeGene = {'A', 'C', 'G', 'T'};
+
+        genes.push({0, startGene});
+
+        int ans = -1;
+
+        while (not genes.empty()) {
+            pair<int, string> curGene = genes.top();
+            genes.pop();
+
+            int mutation = curGene.first;
+            string gene = curGene.second;
+
+            if (gene == endGene) {
+                ans = mutation;
+                break;
+            }
+
+            for (int i = 0; i < gene.size(); i++) {
+                string tempGene = gene;
+                for (int j = 0; j < changeGene.size(); j++) {
+                    tempGene[i] =  changeGene[j];
+
+                    if (allGenes.count(tempGene)) {
+                        allGenes.erase(tempGene);
+                        genes.push({mutation + 1,tempGene});
+                    }
+                }
+            }
         }
-        if (cnt == 1) {
-          dist[v] = dist[u] + 1;
-          q.push(v);
-        }
-      }
+        return ans;
     }
-    if (dist.count(end)) return dist[end];
-    else return -1;
-  }
-}; 
+};
