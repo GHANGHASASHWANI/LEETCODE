85250813ce46d4fb5bd9423ee20e4438f8217283
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-07-01 19:26:18 +0530

    Time: 4 ms (64.38%), Space: 15.2 MB (53.35%) - LeetHub

--- 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.cpp
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.cpp
@@ -2,22 +2,22 @@ class Solution {
 private: 
     vector<vector<int>> paths;
     void findPath(vector<vector<int>> &adjList, int node, vector<int> &path, int destination) {
-        path.push_back(node);
-
         if (node == destination) {
             paths.push_back(path);
+            return;
         }
 
         for (auto &neighbor : adjList[node]) {
+            path.push_back(neighbor);
             findPath(adjList, neighbor, path, destination);
+            path.pop_back();
         }
-        path.pop_back();
     }
 public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         int destination = graph.size() - 1;
         vector<int> path;
-        
+        path.push_back(0);
         findPath(graph, 0, path, destination);
 
         return paths;
