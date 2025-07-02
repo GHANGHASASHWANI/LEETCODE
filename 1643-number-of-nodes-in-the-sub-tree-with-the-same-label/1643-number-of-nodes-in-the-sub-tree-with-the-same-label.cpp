2ed000a6fd49ca652d26f1d3c4a1d6ad29d5babe
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-07-02 18:42:26 +0530

    Time: 127 ms (87.74%), Space: 137.2 MB (89.46%) - LeetHub

--- 1643-number-of-nodes-in-the-sub-tree-with-the-same-label/1643-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
+++ 1643-number-of-nodes-in-the-sub-tree-with-the-same-label/1643-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -1,20 +1,14 @@
 class Solution {
 private:
-    map<char, int> dfs(vector<int> &result, int node, vector<vector<int>> &adjList, string &labels, int parent) {
-        map<char, int> mpp;
-
+    void dfs(vector<int> &result, int node, vector<vector<int>> &adjList, map<char, int> &mpp, string &labels, int parent) {
+        int currCount = mpp[labels[node]];
         for (auto &neighbor : adjList[node]) {
             if (neighbor == parent) continue;
-            map<char, int> curr = dfs(result, neighbor, adjList, labels, node);
-            
-            for (auto &it : curr) {
-                mpp[it.first] += it.second;
-            }
+            dfs(result, neighbor, adjList, mpp, labels, node);
         }
         mpp[labels[node]]++;
-        result[node] += mpp[labels[node]];
-
-        return mpp;
+        // cout<<mpp[labels[node]]<<" "<<currCount<<" "<<node<<endl;
+        result[node] = (mpp[labels[node]] - currCount);
     }
 public:
     vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
@@ -27,7 +21,8 @@ public:
             adjList[vertex1].push_back(vertex2);
             adjList[vertex2].push_back(vertex1);
         }
-        map<char, int> mpp = dfs(allLabel, 0, adjList, labels, -1);
+        map<char, int> mpp;
+        dfs(allLabel, 0, adjList, mpp, labels, -1);
 
         return allLabel;
     }
